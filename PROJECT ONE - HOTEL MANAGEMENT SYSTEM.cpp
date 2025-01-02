08b0a0e9cc4ffb11f4620fb68c8bf520f755a696
Kihuna544 <khalfankhalifa544@gmail.com>
2025-01-02 10:20:51 +0300

    data initialised

--- PROJECT ONE - HOTEL MANAGEMENT SYSTEM.cpp
+++ PROJECT ONE - HOTEL MANAGEMENT SYSTEM.cpp
@@ -6,5 +6,12 @@ int main()
     int quant;
     int choice;
     //Quantity
-   
+   //Quantity
+    int Qrooms = 0, Qpasta = 0, Qbugger = 0, Qnoodles = 0, Qshake = 0, Qchicken = 0;
+
+    //Food item sold
+    int Srooms = 0, Spasta = 0, Sbugger = 0, Snoodles = 0, Sshake = 0, Schicken = 0;
+
+    //Total price of items
+    int Total_rooms = 0, Total_pasta = 0, Total_bugger = 0, Total_noodles = 0, Total_shake = 0, Total_chicken = 0;
  
